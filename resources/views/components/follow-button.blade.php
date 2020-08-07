e5fe81fd4586d8be628562749777351bf24672e9
Seymur Umudov <u.seymur@outlook.com>
2020-08-08 03:21:24 +0400

    add follow button

---
+++ resources/views/components/follow-button.blade.php
@@ -0,0 +1,10 @@
+<form action="{{ route('follow', $user->id) }}" method="post">
+    @csrf
+    <button type="submit" class="bg-blue-400 rounded-full shadow-md py-2 px-4 text-white text-xs">
+        @if (Auth::user()->following($user))
+            Unfollow
+        @else
+            Folow
+        @endif
+    </button>
+</form>
